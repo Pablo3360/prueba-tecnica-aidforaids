798342c46fe7593911ced31d98d48a432b5c6567
Pablo Gomez <pablogomez3360@gmail.com>
2023-03-08 17:14:38 -0300

    Add passwordHash

--- apiServices/users/model.js
+++ apiServices/users/model.js
@@ -1,9 +1,16 @@
+const bcrypt = require('bcrypt');
 const userDao = require('./dao');
 
 module.exports = {
 
   async createUser(user) {
-    return userDao.createUser(user);
+    const saltRaunds = 10;
+    const passwordHash = await bcrypt.hash(user.password, saltRaunds);
+    return userDao.createUser({
+      name: user.name,
+      email: user.email,
+      password: passwordHash,
+    });
   },
 
 };
